e80c80d7575b474dc2f7ab86d710b2117adeb125
Byron Lu <xylu2349@sina.com>
2015-11-18 23:09:37 +0800

    create node layout

---
+++ note/js/main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Road on 2015/11/18.
+ */
+
+
+
+$('.nav-left').onclick = function() {
+    console.log("test");
+};
+
+//$('#img1').addEventListener('click', function(){
+//    console.log("test");
+//});
+
+$('#content').innerHTML = 'Hello!';
+//$('#content').onchange = function() {
+//    console.log("changed");
+//};
